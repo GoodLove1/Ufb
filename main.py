aeb78338938118f9653fee9c307cbab7d81e907d
BalaPriyan <110181533+GoodLove1@users.noreply.github.com>
2024-09-09 21:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,26 @@
 # Developer @JishuDeveloper
 
 from flask import Flask
-import threading 
+import threading
 from bot import Bot
 
 backapp = Flask(__name__)
 
-@backapp.route('/',methods=['GET'])
+@backapp.route('/', methods=['GET'])
 def health_check():
     return 'OK', 200
 
 def run_flask():
-  backapp.run(host=0.0.0.0,port=8000)
+    backapp.run(host="0.0.0.0", port=8000)
 
 def runbot():
-  app = Bot()
-  app.run()
+    app = Bot()
+    app.run()
 
+if __name__ == '__main__':
+    threading.Thread(target=run_flask).start()
+    runbot()
 
-if __name__=='__main__':
-  threading.Thread(target=run_flask).start()
-  runbot()
 
 
 
